f710988e22d3e7eff0f60d946971a83072171ac3
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-29 16:17:31 +0100

    created title and desc for homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <section>
+      <h1 className="text-center mt-5 text-5xl font-extrabold text-black sm:text-6xl">
+        Discover & Share
+        <br />
+        {/* <br className="max-md:hidden" /> */}
+        <span className="text-center orange_gradient ">AI-Powered Prompts</span>
+      </h1>
+      <p className="text-center mt-5 text-lg text-gray-600 sm:text-xl max-w-2xl">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
 };
 
 export default Page;
