f55d1a5936bb1312d32dbed70e17912b2d61cbd4
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-06-06 18:53:22 +0100

    added delete functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -12,24 +12,24 @@ const ProfilePage = () => {
 
   //   EDIT POST FUNCTION
   const handleEdit = (post) => {
-    // router.push(`/update-prompt?id=${post._id}`)
+    router.push(`/update-prompt?id=${post._id}`)
   }
   //   END OF EDIT POST FUNCTION
 
   //   DELETE POST FUNCTION
   const handleDelete = async (post) => {
-    // const hasConfirmed = confirm('Are you sure you want to delete this prompt?')
-    // if (hasConfirmed) {
-    //   try {
-    //     await fetch(`/api/prompt/${post._id.toString()}`, {
-    //       method: 'DELETE',
-    //     })
-    //     const filteredPosts = myPosts.filter((item) => item._id !== post._id)
-    //     setMyPosts(filteredPosts)
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // }
+    const hasConfirmed = confirm('Are you sure you want to delete this prompt?')
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        })
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id)
+        setMyPosts(filteredPosts)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
   //   END OF DELETE POST FUNCTION
 
